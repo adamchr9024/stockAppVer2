92fedfd09cb726a349e6366565078254cf7200b4
adamchr9024 <osha.adams@hotmail.com>
2025-08-09 13:16:03 -0500

    enhancements

--- src/app/table-mat/table-mat.component.ts
+++ src/app/table-mat/table-mat.component.ts
@@ -30,9 +30,9 @@ export class TableMatComponent implements OnInit, AfterViewInit, OnDestroy {
     + Category.Stock + Category.CashAndShortTerm; //ignore money markets
   stocksArray: Array<Security> = [new Security("aapl", 3, 5.67, 5.61, Category.Stock, "4-5.9")]
   tableDataSource: MatTableDataSource<Security>;
-  columnsToDisplay: string[] = ["ticker", "category", "quantity", "market value", "cost basis", "gain/loss", "yahoo price", "52 Week Range", "Price Percentile", "Dividend Yield", "Comment"];
+  columnsToDisplay: string[] = ["ticker", "category", "quantity", "market value", "unit cost", "cost basis", "gain/loss", "yahoo price", "52 Week Range", "Price Percentile", "Dividend Yield", "Comment"];
   //Table columns will be displayed in the same order of values in the array
-  colToDisplay: string[] = ['ticker', 'category', 'quantity', "marketvalue", "costbasis", 'gainloss', 'yahooprice', 'fiftytwowkrng', 'percentage', 'dividendYield', 'comment'];
+  colToDisplay: string[] = ['ticker', 'category', 'quantity', "marketvalue", "unitcost", "costbasis", 'gainloss', 'yahooprice', 'fiftytwowkrng', 'percentage', 'dividendYield', 'comment'];
   @ViewChild(MatSort) sort!: MatSort;
   constructor(private rapidApiService: RapidapiService) {
     this.tableDataSource = new MatTableDataSource(this.stocksArray);
@@ -64,7 +64,7 @@ export class TableMatComponent implements OnInit, AfterViewInit, OnDestroy {
         case "market value": return item.marketvalue;
         case "cost basis": return item.costbasis;
         case "gain/loss": return item.gainloss;
-
+        case "unit cost": return item.unitcost;
 
         // case "ticker": return item.ticker;
         //case "category": return item.category;
