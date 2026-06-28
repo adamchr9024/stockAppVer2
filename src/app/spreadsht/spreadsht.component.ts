bc096e43066a16f8aac2208babe06719e32b5075
chris <osha.adams@hotmail.com>
2026-06-27 19:26:02 -0500

    needs code clean but works

--- src/app/spreadsht/spreadsht.component.ts
+++ src/app/spreadsht/spreadsht.component.ts
@@ -10,10 +10,11 @@ import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { CdkTableModule } from '@angular/cdk/table';
 // import { MatPaginator } from '@angular/material/paginator'
+//import { concatMap } from 'rxjs';
 import { MatSort } from '@angular/material/sort'
-import { Subscription } from 'rxjs'
+import { Subscription, concatMap, from } from 'rxjs'
 import { PercentDirective } from '../percent.directive';
-import { RapidApiGets } from '../../utility/rapidApiGets';
+import { RapidApiGets, SignalServiceGets } from '../../utility/rapidApiGets';
 @Component({
   selector: 'app-spreadsht',
   standalone: true,
@@ -32,21 +33,42 @@ export class SpreadshtComponent implements OnInit, AfterViewInit {
   //Table columns will be displayed in the same order of values in the array
   colToDisplay: string[] = ['ticker', 'quantity', "marketvalue", "unitcost", "costbasis", 'gainloss', 'yahooprice', 'percentage', 'actual_dividend', 'glwdiv', 'glwdvdpct', 'est_annual_income', 'comment'];
   @ViewChild(MatSort) sort!: MatSort;
-  constructor(private utilGets:RapidApiGets) {
+  constructor(private utilRapidGets: RapidApiGets, private utilSignalGet: SignalServiceGets) {
     this.tableDataSource = new MatTableDataSource(this.stocksArray);
 
-    this.subscription =this.signalsService.readSecurities('Stocks.json')
-      .subscribe(next => {
-        next.forEach(val => {
-          this.stocksmap.set(val.ticker, val)
+    utilSignalGet.getSecurityByFileName('Stocks.json', this.stocksmap)
+      .pipe(
+        concatMap(value => {
+          console.log("value", value);
+          return utilRapidGets.getKeys(this.stocksmap, 1.2, "2-3", 4);
         })
-        this.initialize();
+      ).subscribe(doesNothing => {
+
+        console.log("doesNothing", doesNothing, "stockmap", this.stocksmap.size);
+        this.stocksArray = Array.from(this.stocksmap.values());
+        this.tableDataSource.data = this.stocksArray;
       })
+
+    //         utilRapidGets.getKeys(this.stocksmap,1.2,"2-3",4));
+    //.then((holdwaiting) => {
+    // console.log(holdwaiting, "get securities done") //bad logic this is not complete before initialize is called
+    //call get rapidService for Securities price
+    //this.initialize();
+
+    // })
+
+    // this.subscription =this.signalsService.readSecurities('Stocks.json')
+    //   .subscribe(next => {
+    //     next.forEach(val => {
+    //       this.stocksmap.set(val.ticker, val)
+    //     })
+    //     this.initialize();
+    //   })
   }
-  ngOnDestroy(): void {
-    if (this.subscription)
-      this.subscription.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   if (this.subscription)
+  //     this.subscription.unsubscribe();
+  // }
   ngAfterViewInit(): void {
     //throw new Error('Method not implemented.');
     this.tableDataSource.sort = this.sort;
@@ -54,7 +76,7 @@ export class SpreadshtComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     //ADD CODE
     this.tableDataSource.sortingDataAccessor = (item: any, property) => {
-      
+
       switch (property) {
         default: return item[property];
       }
@@ -62,19 +84,19 @@ export class SpreadshtComponent implements OnInit, AfterViewInit {
   }
 
   waiting: string = "fetching ..."
-  signalsService = inject(SignalswatchlistService);
+  //signalsService = inject(SignalswatchlistService); COULD USE FETCH API
   initialize() {
     try {
       this.waiting = "...fetching"; // getKeys(stocksmap: Map<string, Security>, dividendYield:number, fiftyTwoWeekRange:string, regularMarketPrice:number ){
-      this.utilGets.getKeys(this.stocksmap, 2.2, "1-3",4.25)
-      .then((holdwaiting)=>{
-        this.waiting=holdwaiting;
-        this.stocksArray = Array.from(this.stocksmap.values());
-        this.tableDataSource.data = this.stocksArray;
-      }).catch(err=>{
-        this.waiting=err;
-      })
-   }
+      this.utilRapidGets.getKeys(this.stocksmap, 2.2, "1-3", 4.25).subscribe(val => val); //CHECK FOR MEMORY LEAKS
+      // .then((holdwaiting) => {
+      //   this.waiting = holdwaiting;
+      this.stocksArray = Array.from(this.stocksmap.values());
+      this.tableDataSource.data = this.stocksArray;
+      //}).catch(err => {
+      //this.waiting = err;
+      //})
+    }
     catch (err: any) {
       console.error("error caught in spreadsheet initialize", err?.message)
     }
--- src/utility/rapidApiGets.ts
+++ src/utility/rapidApiGets.ts
@@ -1,9 +1,11 @@
 import { Security } from "../model/security";
 import { RapidapiService } from "../app/rapidapi.service";
 //import { of } from "rxjs";
-import { Subscription } from 'rxjs'
+import { take, switchMap, catchError, map } from "rxjs/operators";
+import { Observable, Subscription, of } from 'rxjs'
 import { Injectable, OnInit, OnDestroy } from '@angular/core';
-
+import { SignalswatchlistService } from '../app/signalswatchlist.service';
+import { __await } from "tslib";
 /**
  * get stockmap and the fields to update
  * maybe return a promise when it done or return observable
@@ -13,58 +15,119 @@ import { Injectable, OnInit, OnDestroy } from '@angular/core';
                 updt.yahooprice = val2?.regularMarketPrice;
  */
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class RapidApiGets implements OnDestroy {
-  constructor(private rapidApiService: RapidapiService) {}
-    
-    waiting="";
+    constructor(private rapidApiService: RapidapiService) { }
+
+    waiting = "";
     subscription!: Subscription;
-   getKeys(stocksmap: Map<string, Security>, dividendYield:number, fiftyTwoWeekRange:string, regularMarketPrice:number ){
-      try {
-      //this.waiting = "...fetching";
-      let moresymbols = Array.from(stocksmap.keys());
-      this.subscription = this.rapidApiService.getMutualFundPrices(moresymbols)
-        .subscribe({//unsubscribe please...how to test  
-          next: (n) => {
-            n.forEach((val2: any) => {
-              let updt = stocksmap.get(val2.symbol);
-              if (updt) {
-                updt.dividendYield = val2?.dividendYield;
-                updt.fiftytwowkrng = val2?.fiftyTwoWeekRange;
-                updt.yahooprice = val2?.regularMarketPrice;
-                
-              }
-            })
-          },
-          error: (err) => {
-            console.error("error 'getKeys':", err?.error?.message)
-            this.waiting = "ERROR OCCURRED fetching 'getMutualFundPrices':" + err?.error?.message;
-            return Promise.reject(this.waiting);
-          },
-          complete: () => {
-            this.waiting = 'done';
-            console.log("complete called in getKeys");
-            return Promise.resolve(this.waiting);
-          }
-        })
-      
+    getKeys(stocksmap: Map<string, Security>, dividendYield: number, fiftyTwoWeekRange: string, regularMarketPrice: number) {
+        try {
+            //this.waiting = "...fetching";
+            let moresymbols = Array.from(stocksmap.keys());
+            return this.rapidApiService.getMutualFundPrices(moresymbols)
+                .pipe(
+                    take(1),
+                    switchMap(async (n: any) => {
+                        await n.forEach((val2: any) => {
+                            let updt = stocksmap.get(val2.symbol);
+                            if (updt) {
+                                updt.dividendYield = val2?.dividendYield;
+                                updt.fiftytwowkrng = val2?.fiftyTwoWeekRange;
+                                updt.yahooprice = val2?.regularMarketPrice;
+
+                            }
+                        })
+                        // return n;
+                    }),
+                    map((x: any) => {
+                        return x?.body;
+                    }),
+                    catchError(err => {
+                        console.error("error caught and rethrown in RapidApiGets.getKeys()", err);
+                        throw err;
+                    })
+                )
+
+            //.subscribe({//unsubscribe please...how to test  
+            //     next: (n) => {
+            //         n.forEach((val2: any) => {
+            //             let updt = stocksmap.get(val2.symbol);
+            //             if (updt) {
+            //                 updt.dividendYield = val2?.dividendYield;
+            //                 updt.fiftytwowkrng = val2?.fiftyTwoWeekRange;
+            //                 updt.yahooprice = val2?.regularMarketPrice;
+
+            //             }
+            //         })
+            //     },
+            //     error: (err) => {
+            //         console.error("error 'getKeys':", err?.error?.message)
+            //         this.waiting = "ERROR OCCURRED fetching 'getMutualFundPrices':" + err?.error?.message;
+            //          return of(this.waiting);  //Promise.reject(this.waiting);
+            //     },
+            //     complete: () => {// 
+            //         this.waiting = 'done';
+            //         console.log("complete called in getKeys");
+            //          return of(this.waiting);  //Promise.resolve(this.waiting);
+            //     }
+            // })
+
+        }
+        catch (err: any) {
+            console.error("error caught in RapidApiGets.getKeys()", err?.message);
+            throw err;
+            // this.waiting = "ERROR OCCURRED fetching 'getMutualFundPrices':" + err?.error?.message;
+            // return Promise.reject(this.waiting);
+        }
+        // console.log("should never get here getKeys")
+        // this.waiting = 'done'; //should never get here
+        //return Promise.resolve(this.waiting);
     }
-    catch (err: any) {
-      console.error("error caught in RapidApiGets.getKeys()", err?.message);
-      this.waiting = "ERROR OCCURRED fetching 'getMutualFundPrices':" + err?.error?.message;
-      return Promise.reject(this.waiting);
-    }
-    this.waiting = 'done'; //should never get here
-            return Promise.resolve(this.waiting);
-   }
     ngOnDestroy(): void {
-    if (this.subscription)
-      this.subscription.unsubscribe();
-  }
- 
+        if (this.subscription)
+            this.subscription.unsubscribe();
+    }
+}
 
+@Injectable({
+    providedIn: 'root'
+})
+export class SignalServiceGets implements OnDestroy { //todo maybe return a promise as well
+    constructor(private signalsService: SignalswatchlistService) { }
 
+    //waiting = "";
+    subscription!: Subscription;
+    getSecurityByFileName(fileName: string, stocksmap: Map<string, Security>) {
+        return this.signalsService.readSecurities(fileName)
+            .pipe(
+                take(1),
+                switchMap(async (n: any) => {
+                    await n.forEach((val: any) => {
+                        stocksmap.set(val.ticker, val);
+                    })
+                }),
+                catchError(err => {
+                    console.error("error caught and rethrown in SignalserviceGets()", err);
+                    throw err;
+                })
+            )
+    }
 
+    //console.log("should never get here getSecurityByFileName");
+    //this.waiting = 'done'; //should never get here
+    //return Promise.resolve(this.waiting);
+
+    ngOnDestroy(): void {  //may not want this due to delay of switching between components
+        if (this.subscription)
+            //  console.log("in destroy SingalServiceGets", this.subscription)
+            this.subscription.unsubscribe();
+    }
 
 }
+
+
+
+
+
