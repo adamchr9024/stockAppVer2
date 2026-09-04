0c7739499a10c0d778a9af587d770dd5425c5c6e
adamchr9024 <osha.adams@hotmail.com>
2026-09-04 06:34:38 -0500

    added test

--- src/app/spreadsht/spreadsht.component.spec.ts
+++ src/app/spreadsht/spreadsht.component.spec.ts
@@ -1,19 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { SpreadshtComponent } from './spreadsht.component';
-import { RapidapiService } from '../rapidapi.service';
+//import { RapidapiService } from '../rapidapi.service';
 import { SignalswatchlistService } from '../signalswatchlist.service';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { RapidApiGets, SignalServiceGets } from '../../utility/rapidApiGets';
 describe('SpreadshtComponent', () => {
   let component: SpreadshtComponent;
   let fixture: ComponentFixture<SpreadshtComponent>;
-
+  let signalServiceGet: any;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SpreadshtComponent],
-      providers: [HttpClient, HttpHandler, RapidapiService, SignalswatchlistService, provideAnimations()]
+      providers: [HttpClient, HttpHandler, provideAnimations(), RapidApiGets, SignalServiceGets]
     })
-      .compileComponents();
+      .compileComponents();               //SignalswatchlistService
+    //https://stackoverflow.com/questions/48234781/spyon-service-function-call-in-constructor
+    signalServiceGet = TestBed.get(SignalServiceGets);
+    spyOn(signalServiceGet, 'getSecurityByFileName').and.callThrough();
+
 
     fixture = TestBed.createComponent(SpreadshtComponent);
     component = fixture.componentInstance;
@@ -23,4 +28,9 @@ describe('SpreadshtComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it("should load securities SignalServiceGets", () => {
+    expect(signalServiceGet.getSecurityByFileName).toHaveBeenCalled()
+  })
+
+
 });
